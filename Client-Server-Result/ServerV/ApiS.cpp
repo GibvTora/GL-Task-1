31892faa70b78dc06ee10a1fc91a3b5c6a8e4b2d
GibvTora <140310348+GibvTora@users.noreply.github.com>
2023-09-21 11:26:25 +0300

    Update ApiS.cpp

--- Client-Server-Result/ServerV/ApiS.cpp
+++ Client-Server-Result/ServerV/ApiS.cpp
@@ -8,14 +8,13 @@
 
 const int PROCESS_LIST_BUFFER_SIZE = 4000;
 
-void ConvertWCHARtoChar(const wchar_t* wideStr, char* charStr, int charStrSize) {
-    wcstombs(charStr, wideStr, charStrSize);
+void ConvertWCHARtoChar(const wchar_t* wideStr, char* charStr, int charStrSize) {    //Конвертація wchar_t в char, так як
+    wcstombs(charStr, wideStr, charStrSize);                                         //необхідні функції роботи з процесами працюють з wchar_t, а мережа з char
 }
 
-void GetProcessList(wchar_t* msg, int bufferSize) {
-    // Function retrieves a list of running processes on the system and stores it in 'msg'
+void GetProcessList(wchar_t* msg, int bufferSize) {    //Отримуємо список процессів
     
-#ifdef _WIN32
+#ifdef _WIN32    //На віндовс отримуємо список процесів через snapshot, потім записуємо процес один за одним
     HANDLE hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
     if (hProcessSnap == INVALID_HANDLE_VALUE) {
         // Unable to create a process snapshot
@@ -23,16 +22,16 @@ void GetProcessList(wchar_t* msg, int bufferSize) {
     }
 
     PROCESSENTRY32 pe32;
-    pe32.dwSize = sizeof(PROCESSENTRY32);
+    pe32.dwSize = sizeof(PROCESSENTRY32);    //Визначили розмір
 
     msg[0] = L'\0';
 
     if (Process32First(hProcessSnap, &pe32)) {
-        // Add the first process to the 'msg'
+        //Записали перший процес
         swprintf(msg, bufferSize, L"%d: %s\n", pe32.th32ProcessID, pe32.szExeFile);
 
         while (Process32Next(hProcessSnap, &pe32)) {
-            // Add the next process to the 'msg'
+            //Записали наступні
             wcscat_s(msg, bufferSize, L"\n");
             swprintf(msg + wcslen(msg), bufferSize - wcslen(msg), L"%d: %s", pe32.th32ProcessID, pe32.szExeFile);
         }
@@ -40,27 +39,27 @@ void GetProcessList(wchar_t* msg, int bufferSize) {
 
     CloseHandle(hProcessSnap);
 #else
-    // Iterate through the /proc directory to gather process information
+    //Варіант для лінукс ми ітеруємо по директорії /proc і записуємо інформацію
     DIR* dp = opendir("/proc");
     if (!dp) {
-        // Unable to open the /proc directory
+        //Неможливо вікрити директорію
         return;
     }
 
-    struct dirent* dirp;
+    struct dirent* dirp;    //вказівник на структуру для представлення записів в каталозі
     while ((dirp = readdir(dp)) != NULL) {
-        if (dirp->d_type == DT_DIR) {
-            // Build the path to the cmdline file
+        if (dirp->d_type == DT_DIR) {    
+            //Будуємо шлях до cmdline
             char path[PATH_MAX];
             snprintf(path, sizeof(path), "/proc/%s/cmdline", dirp->d_name);
-
-            FILE* fp = fopen(path, "r");
-            if (fp) {
+            
+            FILE* fp = fopen(path, "r");    //відкриваємо
+            if (fp) {                       //якщо відкрилось то пишемо в cmd
                 char cmd[255];
                 fgets(cmd, sizeof(cmd), fp);
                 fclose(fp);
-                if (strlen(cmd) > 0) {
-                    // Add the process information to 'msg'.
+                if (strlen(cmd) > 0) {      //якщо не 0 то пишемо в msg
+                    //Записуємо інформацію про процес у msg
                     swprintf(msg + wcslen(msg), bufferSize - wcslen(msg), L"%d: %s%s", atoi(dirp->d_name), cmd, wcslen(msg) > 0 ? L"\n" : L"");
                 }
             }
@@ -74,28 +73,28 @@ void GetProcessList(wchar_t* msg, int bufferSize) {
 void HandleClient(SOCKET clientSocket) {
     while (true) {
         wchar_t processListData[PROCESS_LIST_BUFFER_SIZE] = { 0 };
-        GetProcessList(processListData, sizeof(processListData) / sizeof(processListData[0]));
+        GetProcessList(processListData, sizeof(processListData) / sizeof(processListData[0]));    //отримуємо список процесів
 
         char processList[PROCESS_LIST_BUFFER_SIZE];
-        ConvertWCHARtoChar(processListData, processList, sizeof(processList));
+        ConvertWCHARtoChar(processListData, processList, sizeof(processList));    //конвертуємо в char для відправки
 
-        send(clientSocket, processList, sizeof(processList), 0);
+        send(clientSocket, processList, sizeof(processList), 0);    //відправили до клієнта
 
         char buffer[255];
-        int bytesRead = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
-        if (bytesRead > 0) {
-            buffer[bytesRead] = '\0';
+        int bytesRead = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);    //отримуємо від клієнта ID процесу який треба закрити
+        if (bytesRead > 0) {                                                  
+            buffer[bytesRead] = '\0';                                         
 
-            printf("Client sent ID: %s\n", buffer);
+            printf("Client sent ID: %s\n", buffer);                           
 
-            int pid = atoi(buffer);
+            int pid = atoi(buffer);                                           
 
-#ifdef _WIN32
+#ifdef _WIN32    //закриваємо процес для WIN
             bool isProcessTerminated = TerminateProcess(OpenProcess(PROCESS_TERMINATE, FALSE, pid), 0) != 0;
-#else
+#else            //закриваємо процес для Linux
             bool isProcessTerminated = kill(pid, SIGTERM) == 0;
 #endif
-
+                //Відправляємо результат закриття до клієнту
             send(clientSocket, reinterpret_cast<char*>(&isProcessTerminated), sizeof(isProcessTerminated), 0);
 
             if (isProcessTerminated) {
