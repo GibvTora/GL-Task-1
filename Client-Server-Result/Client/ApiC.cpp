73202e764149303f04bdfd8bc303f61016eca4a6
GibvTora <140310348+GibvTora@users.noreply.github.com>
2023-09-21 10:22:13 +0300

    Update ApiC.cpp

--- Client-Server-Result/Client/ApiC.cpp
+++ Client-Server-Result/Client/ApiC.cpp
@@ -2,10 +2,10 @@
 #include <string>
 #include "NetworkingC.h"
 
-const int PROCESS_LIST_BUFFER_SIZE = 4128;
+const int PROCESS_LIST_BUFFER_SIZE = 4128; 
 
 void GetUserInput(SOCKET Connection) {
-    while (true) {
+    while (true) {    //Просте меню
         int choice;
         std::cout << "Menu:" << std::endl;
         std::cout << "1. Get process list" << std::endl;
@@ -14,19 +14,19 @@ void GetUserInput(SOCKET Connection) {
         std::cout << "Choose action: ";
         std::cin >> choice;
 
-        if (choice == 1) {
+        if (choice == 1) {    //Отримати список процесів від серверу та вивести в консоль
             char process_list[PROCESS_LIST_BUFFER_SIZE];
             recv(Connection, process_list, sizeof(process_list), 0);
             std::cout << process_list << std::endl;
         }
-        else if (choice == 2) {
+        else if (choice == 2) {    //Відправити запит на закриття певного процесу
             std::string request;
             std::cout << "Enter process ID to close: ";
             std::cin >> request;
 
             send(Connection, request.c_str(), request.length(), 0);
 
-            bool process_status = false;
+            bool process_status = false;    //Отримали статус закриття процесу
             recv(Connection, reinterpret_cast<char*>(&process_status), sizeof(process_status), 0);
 
             if (process_status) {
